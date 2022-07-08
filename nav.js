a8ac52a36d2bd3dc05cce1ad491608990a638071
Santi543 <santioldani08@gmail.com>
2022-07-07 21:45:05 -0300

    bootstrap

---
+++ nav.js
@@ -0,0 +1,18 @@
+const displayMenu = () =>{
+    const menu = document.getElementById("menu")
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+
+    }else{
+        menu.style.display = "none"
+    }
+}
+
+const displaymenuJs = () =>{
+    const menuJs = document.getElementById("menuJs")
+    if(menuJs.style.display == "none"){
+        menuJs.style.display = "flex"
+    }else{
+        menuJs.style.display = "none"
+    }
+}
